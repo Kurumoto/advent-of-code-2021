264603484741de725c111d83f45c8a7342ce0409
AM <antonickadoo@gmail.com>
2021-12-09 14:38:06 +0000

    Added Day 7

---
+++ Day 7/Part 1 & 2.py
@@ -0,0 +1,8 @@
+with open("input.txt") as f:
+    data = list(sorted(int(n) for n in f.read().split(",")))
+
+median = data[len(data) // 2]
+print(sum(abs(n - median) for n in data))
+
+mean = (sum(data) + 1) // len(data)
+print(sum(abs(n - mean) * (abs(n - mean) + 1) // 2 for n in data))
